26068726d15a6c041cae58d6dcdf259f771b3b15
Nirupamvas-Undru <nirupamvas.undru@zemosolabs.com>
2022-03-17 14:55:42 +0530

    Section 2 files

---
+++ CommentFun.java
@@ -0,0 +1,14 @@
+/*
+    This is multiline comment
+    This is also called a C style comment because it came from C language
+*/
+public class CommentFun {
+    /*
+        Main is the entry point to the application
+     */
+    public static void main(String[] args) {
+        System.out.println("Printing Stuff") ; //print stuff to console
+        //this doesn't print
+        //these are single line comments
+    }// end main
+}
---
+++ VariableFun.java
@@ -0,0 +1,15 @@
+public class VariableFun {
+    public static void main(String[] args) {
+        int myAge; //declaration
+        myAge = 19; //initialization
+
+        final int SOME_NUM = 150;
+
+        String name = "Nirupamvas";
+
+        String myHomeTown = "Hyderabad";
+
+        System.out.println(name + "\tis\t" + myAge);
+        System.out.println("My hometown is " + myHomeTown);
+    }
+}
